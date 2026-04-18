142f62e5d03cb8ef37c5a1c3c97cfa6d0060d752
Mora Vigorito <moraa.mansilla@gmail.com>
2026-04-18 16:28:04 -0300

    Armado Menu inicial
    Creación de la base del menú principal con submenús sin texto.

---
+++ Assets/Scripts/Menu/MenuManager.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0;
+    }
+
+    public void Unpause()
+    {
+        Time.timeScale = 1;
+    }
+}
