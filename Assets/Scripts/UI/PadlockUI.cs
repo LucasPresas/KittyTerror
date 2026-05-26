bf64e2e24cf370955d40f693f7326abae479a81b
Lucas Presas <presaslucas1993@gmail.com>
2026-05-26 19:16:19 +0100

    feat: PadlockUI script with TMPro input

---
+++ Assets/Scripts/UI/PadlockUI.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class PadlockUI : MonoBehaviour
+{
+    [SerializeField] private GameObject panel;
+    [SerializeField] private TMP_InputField inputField;
+    [SerializeField] private Button confirmButton;
+
+    private NumberPadlock _padlock;
+
+    private void Awake()
+    {
+        _padlock = GetComponent<NumberPadlock>();
+        if (_padlock == null) return;
+
+        _padlock.onPlayerEnter.AddListener(Show);
+        _padlock.onPlayerExit.AddListener(Hide);
+
+        if (confirmButton != null)
+            confirmButton.onClick.AddListener(Submit);
+    }
+
+    private void Show()
+    {
+        if (panel != null) panel.SetActive(true);
+        if (inputField != null)
+        {
+            inputField.text = "";
+            inputField.Select();
+        }
+    }
+
+    private void Hide()
+    {
+        if (panel != null) panel.SetActive(false);
+    }
+
+    public void Submit()
+    {
+        if (_padlock == null || inputField == null) return;
+
+        _padlock.CheckCode(inputField.text);
+    }
+}
